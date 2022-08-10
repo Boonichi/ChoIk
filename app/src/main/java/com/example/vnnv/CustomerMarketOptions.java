005d7718ac48d1e2defb7842000d1e570685de2c
Boonichi <pvhung1302@gmail.com>
2022-08-11 00:29:57 +0700

    Login Page

---
+++ app/src/main/java/com/example/vnnv/CustomerMarketOptions.java
@@ -0,0 +1,14 @@
+package com.example.vnnv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMarketOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_market_options);
+    }
+}
