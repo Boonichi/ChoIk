6e4f8dc159ba07fff0614a37ade56e10115c0c42
bao <gb.hoang02@gmail.com>
2022-08-13 04:41:34 +0700

    first push

--- app/src/main/java/com/android/vnnv/LoginPage.java
+++ app/src/main/java/com/android/vnnv/LoginPage.java
@@ -1,5 +1,6 @@
 package com.android.vnnv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -10,7 +11,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.vnnv.model.User;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -24,18 +31,54 @@ public class LoginPage extends AppCompatActivity {
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
+        //Init Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_users =  database.getReference("User");
+
         //LoginButton
         loginbtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                //correct
-                Intent intent = new Intent(LoginPage.this, ChooseRestaurant.class);
-                startActivity(intent);
-            }
-            else
-                //incorrect
-                Toast.makeText(LoginPage.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginPage.this, username.getText().toString(), Toast.LENGTH_SHORT).show();
+
+            table_users.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    //Check if user not exist in database
+                    Toast.makeText(LoginPage.this, username.getText().toString(), Toast.LENGTH_SHORT).show();
+                    System.out.print(username.getText().toString());
+
+                    if (snapshot.child(username.getText().toString()).exists()) {
+                        //Get User information
+                        User user = snapshot.child(username.getText().toString()).getValue(User.class);
+                        if (user.getPass().equals(password.getText().toString())) {
+                            Toast.makeText(LoginPage.this, "Log in successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginPage.this, ChooseRestaurant.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginPage.this, "Log in failed", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginPage.this, "User not exist", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+//            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+//                //correct
+//                Intent intent = new Intent(LoginPage.this, ChooseRestaurant.class);
+//                startActivity(intent);
+//            }
+//            else
+//                //incorrect
+//                Toast.makeText(LoginPage.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+
         }
         });
 
